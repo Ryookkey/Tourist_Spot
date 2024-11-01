14f5cb3f1160d6ece228347be97181462fd2c01d
沖原遼太郎 <okihararyoutarou@okihararyoutarounoMacBook-Pro.local>
2024-11-02 00:25:41 +0900

    adminテーブル追加とGEM導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_01_103544) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_01_145101) do
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "encrypted_password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
